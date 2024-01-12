ff2b65e6aa9961606a561d76785dad0fae90b9da
신승욱 <ukseung.dev@incross.com>
2024-01-12 14:47:22 +0900

    [#19] env: FCM 토큰 수집

--- HappyMoodBoard/AppDelegate/AppDelegate.swift
+++ HappyMoodBoard/AppDelegate/AppDelegate.swift
@@ -6,16 +6,15 @@
 //
 
 import UIKit
+import Firebase
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    let userNotificationCenter = UNUserNotificationCenter.current()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let appearance = UITabBarAppearance()
-        appearance.configureWithTransparentBackground()
-        appearance.backgroundImage = UIImage(named: "tabbar.background")
-        UITabBar.appearance().standardAppearance = appearance
-        UITabBar.appearance().scrollEdgeAppearance = appearance
+        initFirebase(application)
+        initTabbar()
         return true
     }
 
@@ -33,4 +32,67 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+}
+
+extension AppDelegate: MessagingDelegate {
+    func initFirebase(_ application: UIApplication) {
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        userNotificationCenter.delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        
+        userNotificationCenter.requestAuthorization(options: authOptions) { (granted, error) in
+            DispatchQueue.main.async {
+                if granted {
+                    UIApplication.shared.registerForRemoteNotifications()
+                } else {
+                    // Fail case. 수락을 안하는 케이스
+                }
+            }
+        }
+        application.registerForRemoteNotifications()
+    }
+    
+    func initTabbar() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundImage = UIImage(named: "tabbar.background")
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceTokenString = deviceToken.map { String(format: "%02x", $0) }.joined()
+        print("[Log] deviceToken :", deviceTokenString)
+        
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    // https://ios-development.tistory.com/1138
+    // https://fomaios.tistory.com/entry/iOS-푸쉬-알림-탭했을-때-특정-페이지로-이동하기
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+      completionHandler()
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        completionHandler(.newData)
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let fcmToken = fcmToken else {return}
+        print("fcmToken => \(fcmToken)")
+        UserDefaults.standard.set(fcmToken, forKey: "deviceToken")
+    }
 }
